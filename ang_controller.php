111597b994ee1a8755c06464dbbd4134289dbdd2
Usman Haider <haider807@gmail.com>
2015-12-02 12:46:21 +0500

    A sample controller code

--- ang_controller.php
+++ ang_controller.php
@@ -3,11 +3,20 @@
 <script src="angular.min.js"></script>
 <body>
 <?php //include "menu.php";?>
-    <h1>Angular <span style="color: red;">ng-controller</span></h1>
+    <h1>Angular Sample <span style="color: red;">Controller</span></h1>
     <div ng-app="fstApp" ng-controller="fstCont">
         First Name:<input type="text" ng-model="first_name"><br>
-        Last Name:<input type="text" ng-model="last_name">
+        Last Name:<input type="text" ng-model="last_name"><br>
+        <p>Full Name => {{ first_name + " " + last_name }}</p>
     </div>
+   
 
+<script>
+    var app=angular.module('fstApp',[]);
+    app.controller('fstCont',function($scope){
+       $scope.first_name = "Lional";
+       $scope.last_name = "Messi";
+    });
+</script>
 </body>
 </html>
