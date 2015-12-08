a3d027541d1612c6b515c5f426733df6236f7520
Usman Haider <haider807@gmail.com>
2015-12-08 09:30:49 +0500

    Add drag example

---
+++ drag/drag.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<script src="../angular.min.js"></script>
+<body style="margin: 0 300px; background: #ababab;">
+   
+<?php //include "menu.php";?>
+    <h1>Angular Js Sample <span style="color: red ;">Drag</span></h1>
+    <div ng-app="drag">
+        <h2 draggable>Usman Haider</h2>
+        <h3 draggable>Discretelogix</h3>
+        <span draggable>Software Egr.</span>
+    </div>
+    <script src="drag.js"></script>
+</body>
+</html>
+
